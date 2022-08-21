e12b3aa5e93532ae1edd01ea546c25d5f7d23978
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-08-21 11:45:46 +0000

    add

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "efce1d6242544a2eaafcb3a81cbd1e3c.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
