7a0de49e6d788bb01a831f3948cdbc6e13358161
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-08-21 06:57:01 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions',
+  }
+  post '/guests/guest_sign_in', to: 'guests#new_guest'
+
+
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
