0c7413700214b5e257f27ae1a0eaa879846f37b6
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-08-22 12:50:12 +0000

    add

--- db/migrate/20220822115539_create_articles.rb
+++ db/migrate/20220822115539_create_articles.rb
@@ -1,6 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
+      t.integer :user_id,  null: false
+      t.string :title,     null: false
+      t.string :content,   null: false
+      t.string :tag,       null: false
 
       t.timestamps
     end
--- db/migrate/20220822120434_create_bookmarks.rb
+++ db/migrate/20220822120434_create_bookmarks.rb
@@ -1,6 +1,9 @@
 class CreateBookmarks < ActiveRecord::Migration[6.1]
   def change
     create_table :bookmarks do |t|
+      t.integer :user_id,     null: false
+      t.integer :article_id,  null: false
+
 
       t.timestamps
     end
