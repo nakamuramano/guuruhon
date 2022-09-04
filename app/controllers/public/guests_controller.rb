d023684411029e0216f44659ef4af349f5763b37
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-09-04 13:01:25 +0000

    add

--- app/controllers/public/guests_controller.rb
+++ app/controllers/public/guests_controller.rb
@@ -6,7 +6,7 @@ class Public::GuestsController < ApplicationController
      user.name = 'ゲストさん'
    end
    sign_in user
-   redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+   redirect_to root_path, alert: 'ゲストユーザーとしてログインしました。'
  end
 
 end
