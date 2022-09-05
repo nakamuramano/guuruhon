44bcb53591dcbdbb4f546c611bf44328c2a740ef
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-09-05 12:31:43 +0000

    add

--- app/controllers/public/guests_controller.rb
+++ app/controllers/public/guests_controller.rb
@@ -3,7 +3,7 @@ class Public::GuestsController < ApplicationController
  def new_guest
    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
      user.password = SecureRandom.urlsafe_base64
-     user.name = 'ゲストさん'
+     user.name = 'ゲストユーザー'
    end
    sign_in user
    redirect_to root_path, alert: 'ゲストユーザーとしてログインしました。'
--- config/routes.rb
+++ config/routes.rb
@@ -26,11 +26,10 @@ Rails.application.routes.draw do
 
   end
 
-
-
-
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
   }
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
