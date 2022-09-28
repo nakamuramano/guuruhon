89022d19ac24c4154801b895f5b06c4c88ed81fc
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-09-28 11:26:14 +0000

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+before_action :authenticate_user!, except: [:top, :new_guest]
+before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-  protected
+protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
