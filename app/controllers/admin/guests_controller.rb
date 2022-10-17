bdc53f3847888e9a49e956ba1687805f23a858e0
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-10-17 11:57:21 +0000

    ゲストログイン機能修正

--- app/controllers/admin/guests_controller.rb
+++ app/controllers/admin/guests_controller.rb
@@ -1,5 +1,5 @@
 class Admin::GuestsController < ApplicationController
-
+#ゲストログイン機能
  def new_guest
    admin = Admin.find_or_create_by!(email: 'admin@example.com') do |admin|
      admin.password = SecureRandom.urlsafe_base64
