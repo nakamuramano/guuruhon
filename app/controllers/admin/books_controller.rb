5880cef780462b27ee0891f7bdfe82f07bc754a5
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-10-17 11:36:58 +0000

    楽天ブックス検索API 修正

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,10 +1,10 @@
 class Admin::BooksController < ApplicationController
   
 def index
-    @articles = Article.all.page(params[:page]).per(5)
-    @tag_list=Tag.all
-    @tags = Tag.order(created_at: :desc).limit(6)
-  
+  @articles = Article.all.page(params[:page]).per(5)
+  @tag_list=Tag.all
+  @tags = Tag.order(created_at: :desc).limit(6)
+  #楽天ブックス検索API 一覧
   if params[:keyword].present?
     @books = RakutenWebService::Books::Book.search(title: params[:keyword])
   else
