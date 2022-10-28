ea27f922b0c809dcb52fe8dbbedfe89626a18f1a
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-10-28 14:51:20 +0000

    インデント修正

--- app/controllers/public/guests_controller.rb
+++ app/controllers/public/guests_controller.rb
@@ -1,15 +1,15 @@
 class Public::GuestsController < ApplicationController
-before_action :authenticate_user!, except: [:top, :new_guest]
+  before_action :authenticate_user!, except: [:top, :new_guest]
 
-#ゲストログイン機能
- def new_guest
-   user = User.find_or_create_by!(email: 'guest@example.com') do |user|
-     user.password = SecureRandom.urlsafe_base64
-     user.name = 'ゲストユーザー'
-   end
-   sign_in user
-   redirect_to articles_path, alert: 'ゲストユーザーとしてログインしました。'
- end
+  #ゲストログイン機能
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲストユーザー'
+    end
+    sign_in user
+    redirect_to articles_path, alert: 'ゲストユーザーとしてログインしました。'
+  end
 
 
 end
