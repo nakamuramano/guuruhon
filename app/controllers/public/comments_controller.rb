b77cf957dd57c5b385c42948a7ae2c29ab43dbd1
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-10-31 13:18:06 +0000

    remove

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,6 +1,5 @@
 class Public::CommentsController < ApplicationController
 
-before_action :authenticate_user!, except: [:top, :new_guest]
   def create
     article = Article.find(params[:article_id])
     comment = current_user.comments.new(comment_params)
